9abefb9ecc62a03b302a0100e14d888aca5071e7
Jonas Rudi <jrudi@mail.uni-mannheim.de>
2016-02-26 00:55:50 +0100

    removed comments

--- src/parser/exceptions/BadListException.java
+++ src/parser/exceptions/BadListException.java
@@ -1,10 +1,7 @@
 package parser.exceptions;
 
 public class BadListException extends Exception{
-
-	/**
-	 * exception wenn datumslänge != datenlänge
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	
 }
